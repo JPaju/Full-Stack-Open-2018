d03eadd18227e47204a1cf8d1d4da57f589a7f1c
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-03 00:09:20 +0200

    Teht1.2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -11,8 +11,19 @@ const Otsikko = (props) => {
 
 const Sisalto = (props) => {
     return(
+        <div>
+            <Osa osa={props.osa1} tehtavia={props.t1}/>
+            <Osa osa={props.osa2} tehtavia={props.t2}/>
+            <Osa osa={props.osa3} tehtavia={props.t3}/>
+        </div>
+    )
+}
+    
+const Osa = (props) => {
+    return(
         <p>{props.osa}, {props.tehtavia} tehtävää.</p>
     )
+
 }
 
 const Yhteensa = (props) => {
@@ -35,9 +46,7 @@ const App = () => {
     return(
         <div>
             <Otsikko otsikko={kurssi}/>
-            <Sisalto osa={osa1} tehtavia={tehtavia1}/>
-            <Sisalto osa={osa2} tehtavia={tehtavia2}/>
-            <Sisalto osa={osa3} tehtavia={tehtavia3}/>
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} t1={tehtavia1} t2={tehtavia2} t3={tehtavia3}/>
             <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3}/>
         </div>
     )
