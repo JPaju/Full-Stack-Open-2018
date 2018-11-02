6a81223b830f09e5aea86b3e7a8b0104e5ae47e8
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-02 22:01:11 +0200

    Teht1.1

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,22 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Hello = (props) => {
-    const time = new Date();
-    return (
-        
-        <div>
-            <p>Hello {props.name}, age {props.age}!</p>
-            <p>Today is {time.toDateString()}, time is {time.toTimeString()}</p>
-        </div>
+
+const Otsikko = (props) => {
+    return(
+        <h1>{props.otsikko}</h1>
+    )
+}
+
+const Sisalto = (props) => {
+    return(
+        <p>{props.osa}, {props.tehtavia} tehtävää.</p>
+    )
+}
+
+const Yhteensa = (props) => {
+    return(
+        <p>Tehtäviä yhteensä: {props.yhteensa}</p>
     )
 }
 
 const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    
+    const osa1 = 'Reactin perusteet'
+    const osa2 = 'Tiedonvälitys propseilla'
+    const osa3 = 'Komponenttien tila'
+
+    const tehtavia1 = 10
+    const tehtavia2 = 7
+    const tehtavia3 = 14
+
     return(
         <div>
-            <h1>Howdy!</h1>
-            <Hello name="Jaakko" age="24"/>
+            <Otsikko otsikko={kurssi}/>
+            <Sisalto osa={osa1} tehtavia={tehtavia1}/>
+            <Sisalto osa={osa2} tehtavia={tehtavia2}/>
+            <Sisalto osa={osa3} tehtavia={tehtavia3}/>
+            <Yhteensa yhteensa={tehtavia1 + tehtavia2 + tehtavia3}/>
         </div>
     )
 }
