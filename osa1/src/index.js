53ce954dffab8c9ecdd8fa5f43d565696bc17624
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-10 00:58:01 +0200

    Teht1.12

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,99 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-const App = () =>{
-            return(
-            <div>
-                <Palaute/>
-            </div>
-        )   
-}
-
-
-class Palaute extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            feedback : {
-                good: 0,
-                neutral: 0,
-                bad: 0
-            }
+            selected: 0
         }
     }
-    
-    addFeedback = (value) => () => {
-        let oldValue = this.state.feedback
-        oldValue[value] = this.state.feedback[value] +1
-        this.setState({feedback: oldValue})
+
+    nextAnecdote = () => {
+        this.setState({selected: Math.floor(Math.random() * anecdotes.length)})
     }
-    
+
     render() {
-        return(
+        return (
             <div>
-                <div>
-                    <h2>Anna Palautetta</h2>
-                    <Button label='Hyvä' handleClick={this.addFeedback('good')}/>
-                    <Button label='Neutraali' handleClick={this.addFeedback('neutral')}/>
-                    <Button label='Huono' handleClick={this.addFeedback('bad')}/>
-                </div>
-                <div>
-                    <Statistics feedback={this.state.feedback}/>
-                </div>
+                {this.props.anecdotes[this.state.selected]}
+                <br/><Button label="New anecdote" handleClick={this.nextAnecdote}/>
             </div>
         )
-        
     }
 }
-
-class Statistics extends React.Component{
-    constructor(props) {
-        super(props)
-        this.feedback = props.feedback
-    }
-    
-    countAverage = () => {
-        let summa = ((this.feedback.good*1) + (this.feedback.bad * -1))
-        return (summa / this.feedbackCount()).toFixed(1)
-    }
-    
-    positivePercentage = () => {
-        return (this.feedback.good/this.feedbackCount() * 100).toFixed(1)
-    }
-    
-    feedbackCount = () => {
-        return Object.values(this.feedback).reduce((a,b) => {return a + b})
-    }
-    
-    render = () => (
-        <div>
-            <h2>Statistics</h2>
-            {this.feedbackCount() ? (
-                <table>
-                    <tbody>
-                        <Statistic name='Hyvä' value={this.feedback.good}/>
-                        <Statistic name='Neutraali' value={this.feedback.neutral}/>
-                        <Statistic name='Huono' value={this.feedback.bad}/>
-                        <Statistic name='Keskiarvo' value={this.countAverage()}/>
-                        <Statistic name='Positiivisia' value={this.positivePercentage() + '%'}/>
-                    </tbody>
-                </table>
-            ) : (
-                <Display text='Yhtään palautetta ei ole annettu vielä.'/>
-            )}
-        </div>
-    )
-}
-
-const Statistic = ({name, value}) => (
-        <tr>
-            <td><Display text={name}/></td>
-            <td><Display text={value}/></td>
-        </tr>
-)
-
-const Display = ({text}) => <div>{text}</div>
 
 const Button = ({handleClick, label}) => (
     <button onClick={handleClick}>
@@ -102,5 +30,16 @@ const Button = ({handleClick, label}) => (
     </button>
 )
 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <App anecdotes={anecdotes} />,
+    document.getElementById('root')
+)
