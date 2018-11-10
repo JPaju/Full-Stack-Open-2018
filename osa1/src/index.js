ef0b0509c028e56cb28fef95cb70ba746835acb9
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-10 02:57:22 +0200

    Teht1.14

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -6,28 +6,66 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: 0
+            selected: 0,
+            votes: new Array(anecdotes.length).fill(0)
         }
     }
 
-    nextAnecdote = () => {
-        this.setState({selected: Math.floor(Math.random() * anecdotes.length)})
+    clickHandler = (type) => {
+        const handlers = {
+            next: () => {
+                this.setState({ selected: Math.floor(Math.random() * anecdotes.length) })
+            },
+
+            vote: () => {
+                const newVotes = [...this.state.votes]
+                newVotes[this.state.selected] += 1
+                this.setState({ votes: newVotes })
+            }
+        }
+        return handlers[type]
+    }
+
+    findMostVoted = () => {
+        return anecdotes[this.state.votes.indexOf(Math.max(...this.state.votes))]
     }
 
     render() {
         return (
             <div>
-                {this.props.anecdotes[this.state.selected]}
-                <br/><Button label="New anecdote" handleClick={this.nextAnecdote}/>
+                <Anecdote state={this.state} clickHandler={this.clickHandler} />
+                <TopAnecdote mostVoted={this.findMostVoted()} votes={Math.max(...this.state.votes)} />
             </div>
         )
     }
 }
 
-const Button = ({handleClick, label}) => (
-    <button onClick={handleClick}>
-        {label}
-    </button>
+const Anecdote = ({ state, clickHandler }) => (
+    <div>
+        <h1>Anecdote of the hour</h1>
+        <p>{anecdotes[state.selected]}</p>
+        <VoteCountDisplay votes={state.votes[state.selected]} />
+        <Button label="Vote" handleClick={clickHandler('vote')} />
+        <Button label="New anecdote" handleClick={clickHandler('next')} />
+    </div>
+)
+
+const TopAnecdote = ({ mostVoted, votes }) => (
+    <div>
+        <h3>Most voted anecdote</h3>
+        <p>
+            {mostVoted}
+            <VoteCountDisplay votes={votes} />
+        </p>
+    </div>
+)
+
+const Button = ({ handleClick, label }) => (
+    <button onClick={handleClick}>{label}</button>
+)
+
+const VoteCountDisplay = ({ votes }) => (
+    <div>{"Has " + votes + " votes"}</div>
 )
 
 const anecdotes = [
