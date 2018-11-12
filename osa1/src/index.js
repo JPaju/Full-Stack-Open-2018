0d1cf4cf42579d9dd6098a92c9db031f791b7d70
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-12 13:26:09 +0200

    Reformatted Notes-App

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Kurssi from './components/Kurssi'
-import {Otsikko} from './components/utils'
 import './index.css'
 
 
-const App = ({ courses }) => {
+const Note = ({ note }) => {
     return (
-        <div>
-            <div>
-                <Otsikko text={'Kurssit'}/>
-                {courses.map(course => <Kurssi key={course.id} kurssi={course}/>)}
-            </div>
-        </div>
+        <li>
+            {note.content} <strong>{note.important ? 'tärkeä' : ''}</strong>
+        </li>
     )
 }
 
-const kurssit = [
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: props.notes
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Muistiinpanot</h1>
+                <ul>
+                    {this.state.notes.map(note => <Note key={note.id} note={note} />)}
+                </ul>
+            </div>
+        )
+    }
+}
+
+const notes = [
     {
-        nimi: 'Half Stack -sovelluskehitys',
         id: 1,
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10,
-                id: 1
-            },
-            {
-                nimi: 'Tiedonvälitys propseilla',
-                tehtavia: 7,
-                id: 2
-            },
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14,
-                id: 3
-            }
-        ]
+        content: 'HTML on helppoa',
+        date: '2017-12-10T17:30:31.098Z',
+        important: true
     },
     {
-        nimi: 'Node.js',
         id: 2,
-        osat: [
-            {
-                nimi: 'Routing',
-                tehtavia: 3,
-                id: 1
-            },
-            {
-                nimi: 'Middlewaret',
-                tehtavia: 7,
-                id: 2
-            }
-        ]
+        content: 'Selain pystyy suorittamaan vain Javascriptiä',
+        date: '2017-12-10T17:30:31.098Z',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+        date: '2017-11-07T06:43:14.098Z',
+        important: false
     }
 ]
 
-
-ReactDOM.render(<App courses={kurssit} />, document.getElementById('root'));
+ReactDOM.render(<App notes={notes} />, document.getElementById('root'));
