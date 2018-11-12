c7a37f74da2ebdae67e5eb96893da7ab1690f05f
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-12 13:53:24 +0200

    Moved CounterApp to new branch

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,88 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Otsikko = (props) => {
-    return(
-        <h1>{props.kurssi.nimi}</h1>
-    )
-}
+const Otsikko = ({ kurssi }) => (
+    <h1>{kurssi.nimi}</h1>
+)
 
-const Sisalto = (props) => {
-    let osat = props.osat.map(osa => <Osa osa={osa} key={osa.nimi}/>)
+const Sisalto = ({ osat }) => (
+    <div>
+        {osat.map(osa => <Osa osa={osa} key={osa.nimi} />)}
+    </div>
+)
 
-    return(
-        <div>
-            {osat}
-        </div>
-    )
-}
-    
-const Osa = (props) => {
-    return(
-        <li>{props.osa.nimi}, {props.osa.tehtavia} tehtävää.</li>
-    )
+const Osa = (props) => (
+    <li>{props.osa.nimi}, {props.osa.tehtavia} tehtävää.</li>
+)
 
-}
-
-const Yhteensa = (props) => {
-    let tehtavienSumma = 0;
-    props.osat.forEach(osa => {
-       tehtavienSumma += osa.tehtavia;
-   });
-
-    return(
-        <p>Tehtäviä yhteensä: {tehtavienSumma}</p>
-    )
-}
-
-const App = () => {
-    const kurssi = {
-        nimi: 'Half Stack -sovelluskehitys',
-        osat: [
-            {
-                nimi: 'Reactin perusteet',
-                tehtavia: 10
-            },
-            {
-                nimi :'Tiedonvälitys propseilla',
-                tehtavia: 7
-            }, 
-            {
-                nimi: 'Komponenttien tila',
-                tehtavia: 14
-            }
-        ]
-    }
-
-    return(
-        <div>
-           <div className='kurssi'>
-                <Otsikko kurssi={kurssi}/>
-                <Sisalto osat={kurssi.osat}/>
-                <Yhteensa osat={kurssi.osat}/>
-            </div>
-            
-            <div className='counter'>
-                <CounterApp interval={0.5}/>
-            </div>
-        </div>
-    )
-}
+const Yhteensa = ({ osat }) => (
+    <p>Tehtäviä yhteensä: {osat.reduce((summa, osa) => summa += osa.tehtavia)}</p>
+)
 
 class CounterApp extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {counter: 0}
+        this.state = { counter: 0 }
     }
 
-    setCounter = (value) => () => {this.setState({counter: value})}
+    setCounter = (value) => () => { this.setState({ counter: value }) }
 
     render() {
-        return(
+        return (
             <div>
                 <h2>Counter</h2>
-                <div><Display counter={this.state.counter}/></div>
-                
+                <div><Display counter={this.state.counter} /></div>
+
                 <div>
                     <Button
                         handleClick={this.setCounter(this.state.counter + 1)}
@@ -104,9 +54,9 @@ class CounterApp extends React.Component {
     }
 }
 
-const Display = ({counter}) => <div>{counter}</div>
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = ({handleClick, label}) => (
+const Button = ({ handleClick, label }) => (
     <button onClick={handleClick}>
         {label}
     </button>
