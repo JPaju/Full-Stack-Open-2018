6f48369af0a5c9e8c43e13cddb60d094e092016f
Jaakko Paju <JPaju@users.noreply.github.com>
2018-11-12 23:02:48 +0200

    Added feature to prevent duplicate names

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,34 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import Form from './components/Form'
+import PhoneBook from './components/PhoneBook'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      persons: [
-        { name: 'Arto Hellas' }
-      ]
+    constructor(props) {
+        super(props)
+        this.state = {
+            persons: [
+                {
+                    name: 'Arto Hellas',
+                    id: 1
+                }
+            ]
+        }
     }
-  }
 
-  addPerson = (name) => this.setState({persons: [...this.state.persons].concat({name}) })
+    addPerson = (name) => {
+        this.checkDuplicate(name) ?
+        this.setState({ persons: [...this.state.persons].concat({ name }) }) :
+        alert('Name already exists!')
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>Puhelinluettelo</h2>
-        <Form
-            onSubmitCallback={this.addPerson}
-            placeHolder={'Lisättävä nimi'}
-            buttonText='Lisää'
-            />
-        <h2>Numerot</h2>
-        ...
-      </div>
-    )
-  }
+    checkDuplicate = (nameToCheck) =>
+        !this.state.persons.find(person => person.name === nameToCheck)
+
+    render() {
+        return (
+            <div>
+                <h2>Puhelinluettelo</h2>
+                <Form
+                    onSubmitCallback={this.addPerson}
+                    placeHolder={'Lisättävä nimi'}
+                    buttonText='Lisää'
+                />
+                <h2>Numerot</h2>
+                <PhoneBook contacts={this.state.persons} />
+            </div>
+        )
+    }
 }
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
